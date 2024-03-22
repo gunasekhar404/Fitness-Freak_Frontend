afd5da0790be3e009fbd71bd11629fc30ac20cab
Guna Sekhar <gauthamguna2001@gmail.com>
2024-03-22 11:01:40 +0530

    first commit

--- fitnessfreak/src/app/report/[type]/page.tsx
+++ fitnessfreak/src/app/report/[type]/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React from 'react'
 import { LineChart } from '@mui/x-charts/LineChart';
-import {  } from '@mui/x-charts/ChartsXAxis';
 import './ReportPage.css'
 import { AiFillEdit } from 'react-icons/ai'
 import CaloriIntakePopup from '@/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup';
